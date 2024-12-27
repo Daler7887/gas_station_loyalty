e8beb4a18f72c670cf07290767468c15e0ba0e5d
Daler7887 <daler.esn@gmail.com>
2024-12-27 12:15:49 +0500

    deleted background jobs

--- app/scheduled_job/file_cleanup.py
+++ app/scheduled_job/file_cleanup.py
@@ -1,30 +1,30 @@
-import os
-import time
-import logging
-import datetime
+# import os
+# import time
+# import logging
+# import datetime
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-def delete_old_files():
-    folder = "files/car_images"
-    days_old = 30
-    #"""Удаление файлов старше заданного количества дней."""
-    now = time.time()  # Текущее время в секундах с начала эпохи (Unix timestamp)
-    cutoff_time = now - (days_old * 86400)  # Перевод количества дней в секунды (86400 секунд в дне)
+# def delete_old_files():
+#     folder = "files/car_images"
+#     days_old = 30
+#     #"""Удаление файлов старше заданного количества дней."""
+#     now = time.time()  # Текущее время в секундах с начала эпохи (Unix timestamp)
+#     cutoff_time = now - (days_old * 86400)  # Перевод количества дней в секунды (86400 секунд в дне)
 
-    # Проходим по файлам в папке
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
+#     # Проходим по файлам в папке
+#     for filename in os.listdir(folder):
+#         file_path = os.path.join(folder, filename)
         
-        try:
-            if os.path.isfile(file_path):
-                # Получаем время создания файла
-                file_creation_time = os.path.getctime(file_path)
+#         try:
+#             if os.path.isfile(file_path):
+#                 # Получаем время создания файла
+#                 file_creation_time = os.path.getctime(file_path)
 
-                # Если файл старше cutoff_time, удаляем его
-                if file_creation_time < cutoff_time:
-                    os.remove(file_path)
-                    logger.info(f"Файл {file_path} успешно удалён.")
+#                 # Если файл старше cutoff_time, удаляем его
+#                 if file_creation_time < cutoff_time:
+#                     os.remove(file_path)
+#                     logger.info(f"Файл {file_path} успешно удалён.")
     
-        except Exception as e:
-            logger.error(f"Ошибка при удалении {file_path}: {e}")
+#         except Exception as e:
+#             logger.error(f"Ошибка при удалении {file_path}: {e}")
--- app/scheduled_job/updater.py
+++ app/scheduled_job/updater.py
@@ -1,25 +1,25 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from django_apscheduler.jobstores import register_events, DjangoJobStore
-from app.scheduled_job import *
-from app.scheduled_job.file_cleanup import delete_old_files
-from app.refuiling import *
-import logging
+# from apscheduler.schedulers.background import BackgroundScheduler
+# from django_apscheduler.jobstores import register_events, DjangoJobStore
+# from app.scheduled_job import *
+# from app.scheduled_job.file_cleanup import delete_old_files
+# from app.refuiling import *
+# import logging
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    handlers=[logging.StreamHandler()])
+# logging.basicConfig(level=logging.INFO,
+#                     format='%(asctime)s %(levelname)s %(message)s',
+#                     handlers=[logging.StreamHandler()])
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-class jobs:
-    scheduler = BackgroundScheduler(timezone='Asia/Tashkent')
-    scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
+# class jobs:
+#     scheduler = BackgroundScheduler(timezone='Asia/Tashkent')
+#     scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
     
-    #logger.info("Добавление задачи 'process_fuel_sales_log' с интервалом 5 секунд.")
-    #scheduler.add_job(process_fuel_sales_log, 'interval', seconds=10, max_instances=1)
+#     #logger.info("Добавление задачи 'process_fuel_sales_log' с интервалом 5 секунд.")
+#     #scheduler.add_job(process_fuel_sales_log, 'interval', seconds=10, max_instances=1)
     
-    logger.info("Добавление задачи на удаление файлов каждое 1-е число месяца.")
-    scheduler.add_job(delete_old_files, 'cron', day=1, hour=0, minute=0, max_instances=1)
+#     # logger.info("Добавление задачи на удаление файлов каждое 1-е число месяца.")
+#     # scheduler.add_job(delete_old_files, 'cron', day=1, hour=0, minute=0, max_instances=1)
 
-    register_events(scheduler)
-    # logger.info("Планировщик запущен.")
+#     register_events(scheduler)
+#     # logger.info("Планировщик запущен.")
