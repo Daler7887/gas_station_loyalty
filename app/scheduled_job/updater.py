2c512aa7bd24a93a00893bf97477b78a922e93bd
Daler7887 <daler.esn@gmail.com>
2024-12-27 11:22:07 +0500

    disabled shedulejob

--- app/scheduled_job/updater.py
+++ app/scheduled_job/updater.py
@@ -15,8 +15,8 @@ class jobs:
     scheduler = BackgroundScheduler(timezone='Asia/Tashkent')
     scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
     
-    logger.info("Добавление задачи 'process_fuel_sales_log' с интервалом 5 секунд.")
-    scheduler.add_job(process_fuel_sales_log, 'interval', seconds=10, max_instances=1)
+    #logger.info("Добавление задачи 'process_fuel_sales_log' с интервалом 5 секунд.")
+    #scheduler.add_job(process_fuel_sales_log, 'interval', seconds=10, max_instances=1)
     
     logger.info("Добавление задачи на удаление файлов каждое 1-е число месяца.")
     scheduler.add_job(delete_old_files, 'cron', day=1, hour=0, minute=0, max_instances=1)
