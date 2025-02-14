b50bd240ae2f9005966fcfba335b33486f9b284c
Daler7887 <daler.esn@gmail.com>
2025-02-14 15:06:20 +0500

    added logger for plate_recog

--- app/views/plate_recog.py
+++ app/views/plate_recog.py
@@ -21,62 +21,67 @@ class PlateRecognitionView(APIView):
     permission_classes = [AllowAny]  # Отключаем авторизацию
 
     def post(self, request, format=None):
-        # return Response(status=status.HTTP_200_OK)
-        # serializer = CameraDataSerializer(data=request.data)
-        if "parkingSpaceDetection" not in request.data.keys():
-            return Response(status=status.HTTP_200_OK)
-
-        event = json.loads(request.data['parkingSpaceDetection'])
-
-        image_path = None
-        image_path1 = None
-
-        if "backgroundImage" in request.data.keys():
-            background_image = request.data['backgroundImage']
-            background_image_name = f'car_images/{background_image.name}.jpg'
-            image_path = default_storage.save(background_image_name, ContentFile(background_image.read()))
-        
-
-        # plate_number = recognize_plate(base64_image)
-        # plate_number = read_plate(image_path1).upper()
-
-        pump = Pump.objects.filter(ip_address=event['ipAddress']).first()
-        record_exists = PlateRecognition.objects.filter(
-            pump=pump, recognized_at=event['dateTime'][:19]).exists()
-
-        if record_exists:
-            return Response(status=status.HTTP_200_OK)
-
-        # plate_number = recognize_plate(base64_image)
-        plate_number = event['PackingSpaceRecognition'][0]['plateNo']
-        if pump and pump.alpr and "vehicleBodyImage" in request.data.keys():
-            vehicle_body_image = request.data['vehicleBodyImage']
-            vehicle_body_image_name = f'car_images/{vehicle_body_image.name}.jpg'
-            image_path1 = default_storage.save(vehicle_body_image_name, ContentFile(vehicle_body_image.read()))
-            plate_number = read_plate(image_path1).upper()
-
-        event_type = event['PackingSpaceRecognition'][0]['vehicleEnterState']
-        if event_type == 'enter':
-            new_record = PlateRecognition(
-                pump=pump,
-                number=plate_number,
-                image1=image_path,
-                recognized_at=event['dateTime'][:19]
-            )
-            new_record.save()
-        else:
-            record = PlateRecognition.objects.filter(
-                pump=pump,exit_time=None, recognized_at__lte=event['dateTime'][:19]).first()
-            if not record:
+            # return Response(status=status.HTTP_200_OK)
+            # serializer = CameraDataSerializer(data=request.data)
+        try:
+            if "parkingSpaceDetection" not in request.data.keys():
                 return Response(status=status.HTTP_200_OK)
-            record.image2 = image_path
-            record.exit_time = event['dateTime'][:19]
-            record.save()
-            fuel_sale = FuelSale.objects.filter(plate_recognition__isnull=True, created_at__lte=record.exit_time, created_at__gte=record.recognized_at).first()
-            if fuel_sale:
-                fuel_sale.plate_recognition = record
-                fuel_sale.save()
 
+            event = json.loads(request.data['parkingSpaceDetection'])
+
+            image_path = None
+            image_path1 = None
+
+            if "backgroundImage" in request.data.keys():
+                background_image = request.data['backgroundImage']
+                background_image_name = f'car_images/{background_image.name}.jpg'
+                image_path = default_storage.save(background_image_name, ContentFile(background_image.read()))
+
+
+            # plate_number = recognize_plate(base64_image)
+            # plate_number = read_plate(image_path1).upper()
+
+            pump = Pump.objects.filter(ip_address=event['ipAddress']).first()
+            record_exists = PlateRecognition.objects.filter(
+                pump=pump, recognized_at=event['dateTime'][:19]).exists()
+
+            if record_exists:
+                return Response(status=status.HTTP_200_OK)
+
+            # plate_number = recognize_plate(base64_image)
+            plate_number = event['PackingSpaceRecognition'][0]['plateNo']
+            if pump and pump.alpr and "vehicleBodyImage" in request.data.keys():
+                vehicle_body_image = request.data['vehicleBodyImage']
+                vehicle_body_image_name = f'car_images/{vehicle_body_image.name}.jpg'
+                image_path1 = default_storage.save(vehicle_body_image_name, ContentFile(vehicle_body_image.read()))
+                plate_number = read_plate(image_path1).upper()
+
+            event_type = event['PackingSpaceRecognition'][0]['vehicleEnterState']
+            if event_type == 'enter':
+                new_record = PlateRecognition(
+                    pump=pump,
+                    number=plate_number,
+                    image1=image_path,
+                    recognized_at=event['dateTime'][:19]
+                )
+                new_record.save()
+            else:
+                record = PlateRecognition.objects.filter(
+                    pump=pump,exit_time=None, recognized_at__lte=event['dateTime'][:19]).first()
+                if not record:
+                    return Response(status=status.HTTP_200_OK)
+                record.image2 = image_path
+                record.exit_time = event['dateTime'][:19]
+                record.save()
+                fuel_sale = FuelSale.objects.filter(plate_recognition__isnull=True, created_at__lte=record.exit_time, created_at__gte=record.recognized_at).first()
+                if fuel_sale:
+                    fuel_sale.plate_recognition = record
+                    fuel_sale.save()
+
+        except Exception as e:
+            logger.error(f"Internal Server Error: {e}")
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
         try:
             users = Bot_user.objects.filter(car__plate_number=plate_number)
             for user in users:
