cbb65d0049ee6e278ccd6d95d644d7acb239a1a3
Daler7887 <daler.esn@gmail.com>
2025-02-26 16:47:01 +0500

    changed bot url

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    path('', include('bot.urls')),
+    path('bot/', include('bot.urls')),
     path('api/token/', TokenObtainPairView.as_view(),
          name='token_obtain_pair'),  # Логин
     path('api/token/refresh/', TokenRefreshView.as_view(),
