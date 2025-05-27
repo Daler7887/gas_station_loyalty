140ef6fe3b0beb850afbf37eb94d099f8c0e42ea
Daler7887 <daler.esn@gmail.com>
2025-05-27 10:54:50 +0500

    fixed bug

--- app/scheduled_job/jobs.py
+++ app/scheduled_job/jobs.py
@@ -102,7 +102,7 @@ def process_fuel_sales_log():
                     plate_recog = PlateRecognition.objects.filter(pump=pump, recognized_at__gte=timestamp-timedelta(minutes=15), is_processed=False).order_by('-recognized_at').first()
 
                 # Если нет последнего, берём предыдущую продажу
-                if plate_recog is None:
+                if plate_recog is None and timestamp.date() == datetime.now().date():
                     last_sale = FuelSale.objects.filter(pump=pump, date__gte=timestamp-timedelta(minutes=4), date__lte=timestamp).order_by('-date').first()
                     if last_sale:
                         plate_recog = last_sale.plate_recognition
