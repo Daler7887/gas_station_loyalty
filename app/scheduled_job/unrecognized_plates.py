dede1ca849ca0727a0708919bf51b087056ea9e5
Daler7887 <daler.esn@gmail.com>
2025-06-20 18:00:46 +0500

    feat: add logs to background job

--- app/scheduled_job/unrecognized_plates.py
+++ app/scheduled_job/unrecognized_plates.py
@@ -11,6 +11,7 @@ def resolve_unrecognized_plates():
     unrecognized_plate_sales = FuelSale.objects.filter(date__date=datetime.now().date(),plate_recognition__isnull=True, plate_number__isnull=True)
     for sale in unrecognized_plate_sales:
         try:
+            print(f"Обрабатываем sale {sale.id} от {sale.date} колонка {sale.pump.name}")
             plate_recog = PlateRecognition.objects.filter(pump=sale.pump, recognized_at__gte=sale.date-timedelta(minutes=15), recognized_at__lte=sale.date+timedelta(minutes=1),is_processed=False, number__regex=PLATE_NUMBER_TEMPLATE).order_by('-recognized_at').first()
 
             # Если нет подходящих по шаблону, берём последний
