9a8ecac68ad518ebb63fc3a01f6afdf5d2f047da
Daler7887 <daler.esn@gmail.com>
2025-06-25 17:16:24 +0500

    bug(report): fixed bug with query

--- app/utils/queries.py
+++ app/utils/queries.py
@@ -270,7 +270,6 @@ def get_fuel_sales_breakdown_by_pump(start_date, end_date, report_date):
             FROM app_car
             JOIN bot_bot_user ON bot_bot_user.car_id = app_car.id
             WHERE (bot_bot_user.car_id IS NOT NULL AND DATE(bot_bot_user.date) < %s)
-                OR app_car.is_blacklisted = 1
         ),
         registered_today AS (
             SELECT plate_number
