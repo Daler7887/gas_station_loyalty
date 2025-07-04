e781277d6be255aa30026434a8c14b938224fe75
Daler7887 <daler.esn@gmail.com>
2025-07-04 11:33:47 +0500

    feat(notification): add notification for unregistered_users

---
+++ app/management/commands/notify_unregistered.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from app.models import FuelSale
+from datetime import datetime
+import asyncio
+from bot.models import Bot_user
+from bot.utils.bot_functions import send_newsletter, bot
+from bot.services.language_service import get_word
+
+async def notify_unregistered_users():
+    unregistered_users = Bot_user.objects.filter(
+        # car__isnull=True,
+        phone="+998974088009"
+    )
+    async for user in unregistered_users:
+        # Here you would implement the logic to notify the user
+        # For example, sending a message via a bot or email
+        print(f"Notifying user: {user.phone}")
+        print(await send_newsletter(bot, user.chat_id, await get_word("please register", user.lang)))
+        
+
+class Command(BaseCommand):
+    help = 'Notify about unregistered fuel sales'
+
+    def handle(self, *args, **options):
+        # Get all unregistered fuel sales
+        asyncio.run(notify_unregistered_users())
